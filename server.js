417ed66abd6177026a9cd0ba4ca44953a03eee23
Jignesh zala <jigneshkumar1494@gmail.com>
2021-11-03 13:52:34 +0530

    routes on seprate file

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
+// import routes
+const authRoutes = require("./routes/auth");
+
 // app
 const app = express();
 
 // db
 mongoose
   .connect(process.env.DATABASE, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
   })
   .then(() => console.log("DB CONNECTED"))
   .catch((err) => console.log("DB CONNECTION ERR", err));
@@ -21,12 +24,8 @@ app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
 
-// route
-app.get("/api", (req, res) => {
-  res.json({
-    data: "hey you hit node API",
-  });
-});
+// routes middleware
+app.use("/api", authRoutes);
 
 // port
 const port = process.env.PORT || 8000;
