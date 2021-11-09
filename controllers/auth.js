dd7917edd5f07e394e957c734c1ab1b523eed2e5
Jignesh zala <jigneshkumar1494@gmail.com>
2021-11-09 17:30:31 +0530

    create or update user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,34 @@
-exports.createOrUpdateUser = (req, res) => {
-  res.json({
-    data: "hey you hit create-or-update-user API endpoint",
-  });
+const User = require("../models/user");
+
+exports.createOrUpdateUser = async (req, res) => {
+  try {
+    const { name, picture, email } = req.user;
+
+    //Find by Email and update name and picture
+    const user = await User.findOneAndUpdate(
+      { email },
+      { name },
+      { new: true }
+    );
+
+    if (user) {
+      console.log("User Updated", user);
+
+      res.json(user);
+    } else {
+      const newUser = await new User({
+        email,
+        name,
+        picture,
+      }).save();
+      console.log("User Create", newUser);
+
+      res.json(newUser);
+    }
+  } catch (error) {
+    console.log("Error", error);
+    res.status(400).json({
+      err: error,
+    });
+  }
 };
